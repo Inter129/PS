e88026944f77a1c7b2255cf13283ed8c132c0ec8
inter129 <sangho129@icloud.com>
2023-10-11 22:14:06 +0900

    first commit

---
+++ helpers/template.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+#include <climits>
+
+int x_move[] = {0, 0, 1, -1}, y_move[] = {1, -1, 0, 0};
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+}
