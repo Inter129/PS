abb7a374b2f52b1f8a4919a06b1a225e48604625
inter129 <sangho129@icloud.com>
2023-10-13 18:13:44 +0900

    solve: acmicpc / 2751.cpp

---
+++ acmicpc/27__/2751.cpp
@@ -0,0 +1,46 @@
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <iostream>
+#include <map>
+#include <set>
+#include <math.h>
+#include <numeric>
+#include <cassert>
+#include <stack>
+#include <cstring>
+#include <climits>
+
+int x_move[] = {0, 0, 1, -1}, y_move[] = {1, -1, 0, 0};
+
+typedef long long ll;
+typedef unsigned long ull;
+
+#define interface struct
+
+typedef signed char i8;
+typedef short i16;
+typedef int i32;
+typedef long long i64;
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int N, t;
+    cin >> N;
+    vector<int> v;
+    for(int i=0; i<N; i++){
+        cin >> t;
+        v.push_back(t);
+    }
+    sort(v.begin(), v.end(), [](int a, int b){
+        return a < b;
+    });
+    for(int i=0; i<N; i++){
+        cout << v[i] << '\n';
+    }
+}
